b385a133c24714fe6e3e721370a6f5cc369035c0
blubMe <fahmiirsyad10@gmail.com>
2018-05-23 09:21:23 +0700

    sass support

--- meta.js
+++ meta.js
@@ -44,6 +44,10 @@ module.exports = {
       "type": "confirm",
       "message": "Install vue-router?"
     },
+    "sass": {
+      "type": "confirm",
+      "message": "Install Sass?"
+    },
     "lint": {
       "type": "confirm",
       "message": "Use ESLint to lint your code?"
