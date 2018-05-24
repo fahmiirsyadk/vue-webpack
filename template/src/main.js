9d6f70a6ce71349151ae22db7efda84ee954cfcb
blubMe <fahmiirsyad10@gmail.com>
2018-05-24 08:23:10 +0700

    Update Vuefire config

--- template/src/main.js
+++ template/src/main.js
@@ -3,11 +3,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{#firebase}}
+import VueFire from 'vuefire'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/firebase}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
 
+{{#firebase}}
+Vue.use(VueFire){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+{{/firebase}}
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
