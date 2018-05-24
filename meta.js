711d50469333259e1a8fe3b48fa5aef6d03ba407
blubMe <fahmiirsyad10@gmail.com>
2018-05-24 11:15:52 +0700

    Update Firebase files config

--- meta.js
+++ meta.js
@@ -100,7 +100,8 @@ module.exports = {
     "test/unit/**/*": "unit",
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+    "src/router/**/*": "router",
+    "src/server/firebase.js": "firebase"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
---
+++ template/src/server/firebase.js
@@ -0,0 +1,8 @@
+import { firebase } from "@firebase/app";
+import "@firebase/firestore";
+
+const firebaseApp = firebase.initializeApp({
+    // your firebase config goes here
+});
+
+export const db = firebaseApp.firestore();
