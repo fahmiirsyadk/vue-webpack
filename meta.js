b203ca12eae5f9860dddc58f2943e0d6ff34230b
blubMe <fahmiirsyad10@gmail.com>
2018-05-24 11:30:00 +0700

    Add Vuex config on meta

--- meta.js
+++ meta.js
@@ -101,7 +101,8 @@ module.exports = {
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router",
-    "src/server/firebase.js": "firebase"
+    "src/server/firebase.js": "firebase",
+    "src/store/**/*": "vuex",
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
